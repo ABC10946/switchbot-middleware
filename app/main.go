2f60da329bdaa85a19b5a6d2e73e7e53c2e675ae
ABC10946 <abctech10946@gmail.com>
2024-10-20 22:00:50 +0900

    ライト点灯実験用コード

--- app/main.go
+++ app/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
 	"time"
+
+	"io"
 
 	"github.com/google/uuid"
 )
@@ -45,8 +50,92 @@ func main() {
 	apiHeader["sign"] = sign
 	apiHeader["nonce"] = nonce.String()
 
-	// Print API header
-	for key, value := range apiHeader {
-		fmt.Printf("%s: %s\n", key, value)
+	// request switchbot api with api header
+
+	/*
+
+		req, err := http.NewRequest("GET", "https://api.switch-bot.com/v1.1/devices", nil)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		// Add headers to request
+		for key, value := range apiHeader {
+			req.Header.Add(key, value)
+		}
+
+		// Create http client
+		client := &http.Client{}
+
+		// Send request
+		resp, err := client.Do(req)
+
+		defer resp.Body.Close()
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		// Read all response body
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		// Print response body
+		fmt.Println(string(body))
+
+	*/
+
+	// turn off switchbot color bulb
+
+	// POST /v1.1/devices/{deviceId}/commands
+
+	deviceId := "01-202304012328-87495896"
+
+	// Build API Request Body
+	apiRequestBody := map[string]string{
+		"command":     "turnOn",
+		"parameter":   "default",
+		"commandType": "command",
 	}
+
+	// convert request body to json
+	requestBody, err := json.Marshal(apiRequestBody)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// request switchbot api with api header
+	req, err := http.NewRequest("POST", fmt.Sprintf("https://api.switch-bot.com/v1.1/devices/%s/commands", deviceId), bytes.NewBuffer(requestBody))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// add headers to request
+	for key, value := range apiHeader {
+		req.Header.Add(key, value)
+	}
+
+	// add request body to request
+	client := &http.Client{}
+
+	// send request
+
+	res, err := client.Do(req)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer res.Body.Close()
+
+	// read all response body
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// print response body
+	fmt.Println(string(body))
 }
